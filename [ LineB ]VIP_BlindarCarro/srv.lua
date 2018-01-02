829f045b282768efd14fbe8cf1cc104870ad9bd0
Lucas <lucas.matheus0104@gmail.com>
2018-01-02 02:53:07 -0200

    Update 0.1
    - Fix mirror error's

--- [ LineB ]VIP_BlindarCarro/srv.lua
+++ [ LineB ]VIP_BlindarCarro/srv.lua
@@ -1,6 +1,7 @@
     function vehicleGodMod(player)
         if not (hasObjectPermissionTo(player,"general.adminpanel",false)) then
-        outputChatBox("Comando exclusivo, você não tem permissão!",player,255) return end
+        outputChatBox("Comando exclusivo, você não tem permissão!",player,255) 
+            return end
         if (isVehicleDamageProof(getPedOccupiedVehicle(player))) then
         setVehicleDamageProof(getPedOccupiedVehicle(player),false)
         outputChatBox("Veiculo Blindado, Desativado!",player,255)
